04719ecaa73101e2eead161b60c06705367bd8cf
rubenhelsloot <rht670@student.vu.nl>
2016-05-06 19:10:20 +0200

    Added scripts

---
+++ Combinatorisch/runInstance.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+i=$1
+echo "Instance $i"
+java -jar CVRPTW.jar Instances/Instance$i.txt Results/Results_Instance$i.txt
+python Results/Validator/SolutionCVRPTWUI.py -s Results/Results_Instance$i.txt -i Instances/Instance$i.txt
